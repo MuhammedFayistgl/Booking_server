92ae95a9fccdd2f3ffe0b2a3b7836310855219a9
MuhammedFayistgl <77223454+MuhammedFayistgl@users.noreply.github.com>
2023-08-29 18:05:53 +0530

    error

--- server/server.js
+++ server/server.js
@@ -1,23 +1,46 @@
 import mongoose from "mongoose";
-
+import express from "express";
+const app = express();
 
 const connution = async () => {
-  try {
-    mongoose.set('strictQuery', true)
-    mongoose.connect(
-      // "mongodb+srv://booking:booking@cluster0.qdxkopx.mongodb.net/?retryWrites=true&w=majority"
-      "mongodb+srv://booking:booking@cluster0.qdxkopx.mongodb.net/?retryWrites=true&w=majority"
-      // "mongodb://127.0.0.1:27017"
-      , {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-      });
+  mongoose.set('strictQuery', true)
+  mongoose.connect(
+    process.env.MONGODB_DERVER_IP,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }, (err) => {
+      if (err) {
+        console.log("error in connection");
+        process.exit(1)
+      } else {
+        console.log("mongodb is connected");
+      }
+    });
+  //create a server object:
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`nodes listening on port${process.env.PORT} `);
+  });
+  //the server object listens on port 8080
 
-    console.log(`Mongodb connection established`);
-  } catch (error) {
-    console.log("connuction Erorr", error);
-    process.exit(1);
-  }
+
+  // try {
+  //   mongoose.set('strictQuery', true)
+  //   mongoose.connect(
+  //     process.env.MONGODB_DERVER_IP
+  //     // "mongodb+srv://booking:booking@cluster0.qdxkopx.mongodb.net/?retryWrites=true&w=majority"
+  //     // "mongodb+srv://booking:booking@cluster0.qdxkopx.mongodb.net/?retryWrites=true&w=majority"
+  //     // "mongodb://127.0.0.1:27017"
+  //     , {
+  //       useNewUrlParser: true,
+  //       useUnifiedTopology: true
+  //     });
+
+  //   console.log(`Mongodb connection established`);
+  // } catch (error) {
+  //   console.log("connuction Erorr", error);
+  //   process.exit(1);
+  // }
 };
 
 
