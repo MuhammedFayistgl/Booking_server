26aee7321dcd6cc9217cbc83a34740746f585f9f
MuhammedFayistgl <77223454+MuhammedFayistgl@users.noreply.github.com>
2023-08-30 16:19:47 +0530

    Change cors link remove

--- index.js
+++ index.js
@@ -11,22 +11,22 @@ import cookieParser from "cookie-parser";
 
 
 const app = express();
-// app.use(cors())
+app.use(cors())
 
-app.use(function (req, res, next) {
+// app.use(function (req, res, next) {
 
-  var allowedDomains = ['http://localhost:5173', 'http://localhost:5174','https://muhammedfayistgl.github.io/Booking_frontend/#/'];
-  var origin = req.headers.origin;
-  if (allowedDomains.indexOf(origin) > -1) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-  }
+//   var allowedDomains = ['http://localhost:5173', 'http://localhost:5174','https://muhammedfayistgl.github.io/Booking_frontend/#/'];
+//   var origin = req.headers.origin;
+//   if (allowedDomains.indexOf(origin) > -1) {
+//     res.setHeader('Access-Control-Allow-Origin', origin);
+//   }
 
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Accept');
-  res.setHeader('Access-Control-Allow-Credentials', true);
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Accept');
+//   res.setHeader('Access-Control-Allow-Credentials', true);
 
-  next();
-})
+//   next();
+// })
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.json({ extended: false }));
